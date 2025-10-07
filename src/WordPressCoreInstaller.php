9573b5d7a10b44a958afdcfeb345370d07df7453
Alf Drollinger <40421928+adrolli@users.noreply.github.com>
2025-10-08 00:24:09 +0200

    Update WordPressCoreInstaller.php

--- src/WordPressCoreInstaller.php
+++ src/WordPressCoreInstaller.php
@@ -19,7 +19,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-namespace Roots\Composer;
+namespace Moox\Composer;
 
 use Composer\Config;
 use Composer\Installer\LibraryInstaller;
